b7b215029ed85a36ad06a5a5d33173037906b1e6
Nabeel Kirmani <38636337+nabeelkirmani@users.noreply.github.com>
2024-12-02 15:33:57 +0300

    deleted files by mistake in 2nd last commit. no change in functionality since 2nd last commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+// src/components/Navbar/Navbar.jsx
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      {/* TODO add logo or icons here */}
+      <h2 className="navbar-title">☰</h2>
+    </div>
+  );
+}
+
+export default Navbar;
