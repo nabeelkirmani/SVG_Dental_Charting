cf640718cfce9a1ff399ea675ef39f56991d6127
Nabeel Kirmani <38636337+nabeelkirmani@users.noreply.github.com>
2024-12-11 13:03:30 +0300

    split ToothWheel into two rows

--- src/components/ToothWheel/ToothWheel.jsx
+++ src/components/ToothWheel/ToothWheel.jsx
@@ -3,10 +3,10 @@ import React, { useContext } from "react";
 import "./ToothWheel.scss";
 import { SelectionContext } from "../../contexts/SelectionContext.jsx";
 
-const teethNumbers = [
-  // Upper teeth numbers
+const upperTeeth = [
   18, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 28,
-  // Lower teeth numbers
+];
+const lowerTeeth = [
   38, 37, 36, 35, 34, 33, 32, 31, 41, 42, 43, 44, 45, 46, 47, 48,
 ];
 
@@ -15,13 +15,32 @@ const ToothWheel = () => {
 
   return (
     <div className="tooth-wheel">
-      <ol className="tooth-wheel-navigation">
-        {teethNumbers.map((number) => (
-          <li key={number} className={selectedTooth === number ? "active" : ""}>
-            <button onClick={() => handleToothSelect(number)}>{number}</button>
-          </li>
-        ))}
-      </ol>
+      <div className="tooth-wheel-navigation">
+        <ol className="upper-teeth">
+          {upperTeeth.map((number) => (
+            <li
+              key={number}
+              className={selectedTooth === number ? "active" : ""}
+            >
+              <button onClick={() => handleToothSelect(number)}>
+                {number}
+              </button>
+            </li>
+          ))}
+        </ol>
+        <ol className="lower-teeth">
+          {lowerTeeth.map((number) => (
+            <li
+              key={number}
+              className={selectedTooth === number ? "active" : ""}
+            >
+              <button onClick={() => handleToothSelect(number)}>
+                {number}
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 };
